47f3ea0a164af31e50cd9b659c567fb7908fd8da
Gangplank <joonjaebang@gmail.com>
2016-10-14 19:30:19 -0400

    Test Unity Proj
    blah

---
+++ TD_Test/Assets/Script_ENYStats.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script_ENYStats : MonoBehaviour {
+    public int health = 10;
+    public int damage = 3;
+    public float atkSpeed = 2.0f;
+    public float speed = 5.0f;
+}
